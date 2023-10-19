646145d8c3e8683044b1e8e30173000f6eb8a80a
Carlos Rafael Ferreira Pereira <carlosrafael@live.com.pt>
2023-10-19 12:50:26 +0100

    video 129

--- Bulky.DataAccess/Data/AplicationDbContext.cs
+++ Bulky.DataAccess/Data/AplicationDbContext.cs
@@ -16,6 +16,8 @@ namespace Bulky.DataAcess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -27,6 +29,40 @@ namespace Bulky.DataAcess.Data
                 new Category { ID = 2, DisplayOrder = 2, Name = "SciFi" },
                 new Category { ID = 3, DisplayOrder = 3, Name = "History" }
                 );
+
+            modelBuilder.Entity<Company>().HasData(
+                 new Company
+                 {
+                     ID = 1,
+                     Name = "Tech Solution",
+                     StreetAddress = "123 Tech St",
+                     City = "Tech City",
+                     PostalCode = "12121",
+                     State = "IL",
+                     PhoneNumber = "6669990000"
+                 },
+                 new Company
+                 {
+                     ID = 2,
+                     Name = "Vivid Books",
+                     StreetAddress = "999 Vid St",
+                     City = "Vid City",
+                     PostalCode = "66666",
+                     State = "IL",
+                     PhoneNumber = "7779990000"
+                 },
+                 new Company
+                 {
+                     ID = 3,
+                     Name = "Readers Club",
+                     StreetAddress = "999 Main St",
+                     City = "Lala land",
+                     PostalCode = "99999",
+                     State = "NY",
+                     PhoneNumber = "1113335555"
+                 }
+                 );
+
 
             modelBuilder.Entity<Product>().HasData(
                 new Product
@@ -41,7 +77,7 @@ namespace Bulky.DataAcess.Data
                     Price50 = 85,
                     Price100 = 80,
                     CategoryID = 1,
-                    ImageUrl =""
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -68,12 +104,12 @@ namespace Bulky.DataAcess.Data
                     Price = 50,
                     Price50 = 40,
                     Price100 = 35,
-                    CategoryID = 2,
+                    CategoryID = 1,
                     ImageUrl = ""
                 },
                 new Product
                 {
-                    ID = 4,
+                    ID   = 4,
                     Title = "Cotton Candy",
                     Author = "Abby Muscles",
                     Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
@@ -96,7 +132,7 @@ namespace Bulky.DataAcess.Data
                     Price = 27,
                     Price50 = 25,
                     Price100 = 20,
-                    CategoryID = 3,
+                    CategoryID = 2,
                     ImageUrl = ""
                 },
                 new Product
@@ -110,9 +146,8 @@ namespace Bulky.DataAcess.Data
                     Price = 23,
                     Price50 = 22,
                     Price100 = 20,
-                    CategoryID = 1,
+                    CategoryID = 3,
                     ImageUrl = ""
-
                 }
                 );
         }
---
+++ Bulky.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.DataAcess.Data;
+using Bulky.DataAcess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAcess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private AplicationDbContext _db;
+        public CompanyRepository(AplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}

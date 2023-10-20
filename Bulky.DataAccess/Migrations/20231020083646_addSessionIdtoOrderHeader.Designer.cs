bb7ce8a2ffbc71eef51ec68378323895bd16140b
Carlos Rafael Ferreira Pereira <carlosrafael@live.com.pt>
2023-10-20 18:14:16 +0100

    aula 167

--- Bulky.DataAccess/Migrations/20231020083646_addSessionIdtoOrderHeader.Designer.cs
+++ Bulky.DataAccess/Migrations/20231020083646_addSessionIdtoOrderHeader.Designer.cs
@@ -4,6 +4,7 @@ using Bulky.DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bulky.DataAcess.Migrations
 {
     [DbContext(typeof(AplicationDbContext))]
-    partial class AplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231020083646_addSessionIdtoOrderHeader")]
+    partial class addSessionIdtoOrderHeader
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -222,7 +225,7 @@ namespace Bulky.DataAcess.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("TrackingNumber")
+                    b.Property<string>("TransactionNumber")
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
---
+++ Bulky.DataAccess/Migrations/20231020141542_ModifyOrderHEaderTTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyOrderHEaderTTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransactionNumber",
+                table: "OrderHeaders",
+                newName: "TrackingNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TrackingNumber",
+                table: "OrderHeaders",
+                newName: "TransactionNumber");
+        }
+    }
+}
